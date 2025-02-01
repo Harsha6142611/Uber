34a3fb9515d284d3c73f6125d507ac6b0421331f
Harsha6142611 <pasupulaharsha7006@gmail.com>
2025-02-01 14:37:44 +0530

    users routes

--- Backend/app.js
+++ Backend/app.js
@@ -6,6 +6,8 @@ const app = express();
 const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes');
 const captainRoutes = require('./routes/captain.routes');
+const mapsRoutes = require('./routes/maps.routes');
+const rideRoutes = require('./routes/ride.routes');
 const cookieParser = require('cookie-parser');
 
 connectToDb();//DB connection
@@ -21,6 +23,6 @@ app.get('/', (req, res) => {
 
 
 app.use('/users', userRoutes);
-app.use('/captains', captainRoutes);
+
 
 module.exports = app;
