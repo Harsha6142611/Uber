18d77fe605c7f360e38d5601e51f307faefd205a
Harsha6142611 <pasupulaharsha7006@gmail.com>
2025-02-01 14:46:52 +0530

    get-distance-time route is added

--- Backend/routes/maps.routes.js
+++ Backend/routes/maps.routes.js
@@ -10,6 +10,14 @@ router.get('/get-coordinates',
     mapController.getCoordinates
 );
 
+router.get('/get-distance-time',
+    query('origin').isString().isLength({ min: 3 }),
+    query('destination').isString().isLength({ min: 3 }),
+    authMiddleware.authUser,
+    mapController.getDistanceTime
+)
+
+
 
 
 module.exports = router;
